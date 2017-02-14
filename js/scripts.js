5626f191af2e17dbe3bb47b9952106ffed92a697
Clifford Grimmell and Cameron Jensen <cameron@>
2017-02-14 15:17:37 -0800

    Flashcards hide header when definition appears

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+$(function(){
+  $(".clickable").click(function () {
+    $("#row1column1a").toggle();
+    $("#row1column1").toggle();
+  });
+  $(".clickable1").click(function () {
+    $("#row1column2").toggle();
+    $("#row1column2a").toggle();
+  });
+  $(".clickable2").click(function () {
+    $("#row1column3").toggle();
+    $("#row1column3a").toggle();
+  });
+  $(".clickable3").click(function () {
+    $("#row1column4").toggle();
+    $("#row1column4a").toggle();
+  });
+  $(".clickable4").click(function () {
+    $("#row2column1").toggle();
+    $("#row2column1a").toggle();
+  });
+  $(".clickable5").click(function () {
+    $("#row2column2").toggle();
+    $("#row2column2a").toggle();
+  });
+  $(".clickable6").click(function () {
+    $("#row2column3").toggle();
+    $("#row2column3a").toggle();
+  });
+  $(".clickable7").click(function () {
+    $("#row2column4").toggle();
+    $("#row2column4a").toggle();
+  });
+  $(".clickable8").click(function () {
+    $("#row3column1").toggle();
+    $("#row3column1a").toggle();
+  });
+  $(".clickable9").click(function () {
+    $("#row3column2").toggle();
+    $("#row3column2a").toggle();
+  });
+  $(".clickable10").click(function () {
+    $("#row3column3").toggle();
+    $("#row3column3a").toggle();
+  });
+  $(".clickable11").click(function () {
+    $("#row3column4").toggle();
+    $("#row3column4a").toggle();
+  });
+  $(".clickable12").click(function () {
+    $("#row4column1").toggle();
+    $("#row4column1a").toggle();
+  });
+  $(".clickable13").click(function () {
+    $("#row4column2").toggle();
+    $("#row4column2a").toggle();
+  });
+  $(".clickable14").click(function () {
+    $("#row4column3").toggle();
+    $("#row4column3a").toggle();
+  });
+  $(".clickable15").click(function () {
+    $("#row4column4").toggle();
+    $("#row4column4a").toggle();
+  });
+  $(".clickable16").click(function () {
+    $("#row5column1").toggle();
+    $("#row5column1a").toggle();
+  });
+  $(".clickable17").click(function () {
+    $("#row5column2").toggle();
+    $("#row5column2a").toggle();
+  });
+  $(".clickable18").click(function () {
+    $("#row5column3").toggle();
+    $("#row5column3a").toggle();
+  });
+  $(".clickable19").click(function () {
+    $("#row5column4").toggle();
+    $("#row5column4a").toggle();
+  });
+});
